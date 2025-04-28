f78fbfa5c10acec93cb924e2fa6d171d5cbc6573
Hiru-M <120943301+Hiru-M@users.noreply.github.com>
2025-04-28 07:09:32 +0530

    Add files via upload

---
+++ script.jsx
@@ -0,0 +1,7 @@
+var typed= new Typed(".textc", {
+    strings: ["A AI/ML enthusiastic" , "QA enthusiastic" , "A Project Mangement"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
